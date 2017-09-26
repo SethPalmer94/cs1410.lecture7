0d5985c371bf20848e765b6fb4841b4c1c60f053
sethpalmer94 <sethpalmer@mail.weber.edu>
2017-09-26 12:15:04 -0600

    update 2

--- DistancelassArgument/DistancelassArgument/functions.cpp
+++ DistancelassArgument/DistancelassArgument/functions.cpp
@@ -27,19 +27,32 @@ public:
 	{
 		cout << "feet: " << feet << " and inches: " << inches << endl;
 	}
-	void add_dist(Distance d1, Distance d2)
-	{
-
-	}
+	void add_dist(Distance d1, Distance d2);
+	
 };
 int main()
 {
 	Distance d1, d2(1,15);
 	d1.showDist();
 	d2.showDist();
+	Distance d3(2, 4), d4;
+	d4.add_dist(d2, d3);
+	d3.showDist();
+	d4.showDist();
 
 
 
 	system("pause");//REMOVE BEFORE SUBMIT
 	return 0;
+}
+void Distance::add_dist(Distance d1, Distance d2)
+{
+	inches = d2.inches + d1.inches;
+	feet = 0; 
+	if (inches >= 12.0)
+	{
+		inches -= 12.0;
+		feet++;
+	}
+	feet += d2.feet + d1.feet;
 }
